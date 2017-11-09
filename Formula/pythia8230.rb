16486e068f570d03f7765580fdde1cef1c6c89aa
Joel Doss <joel.s.doss@utexas.edu>
2017-11-09 15:30:00 -0800

    Added Pythia8.230

---
+++ Formula/pythia8230.rb
@@ -0,0 +1,36 @@
+class Pythia8230 < Formula
+  desc "Monte Carlo event generator"
+  homepage "https://pythia8.hepforge.org"
+  url "http://home.thep.lu.se/~torbjorn/pythia8/pythia8230.tgz"
+  version "8.230"
+  sha256 "8ff4281bce9d22b17f1ec88e9e9353e8172512d5ef96296ace9486860e9ed2c9"
+
+  depends_on "hepmc"
+  depends_on "fastjet"
+  depends_on "lhapdf"
+  depends_on "boost"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --enable-shared
+      --with-hepmc2=#{Formula["hepmc"].opt_prefix}
+      --with-fastjet3=#{Formula["fastjet"].opt_prefix}
+      --with-lhapdf6=#{Formula["lhapdf"].opt_prefix}
+      --with-boost=#{Formula["boost"].opt_prefix}
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    ENV["PYTHIA8DATA"] = share/"Pythia8/xmldoc"
+
+    cp_r share/"Pythia8/examples/.", testpath
+    system "make", "main01"
+    system "./main01"
+    system "make", "main41"
+    system "./main41"
+  end
+end
