735c88fc89258607b3e69f9b9ad7345945944888
Joel Doss <joel.s.doss@utexas.edu>
2019-05-28 12:02:08 -0700

    Modified FeynRules

--- Formula/feyn_rules.rb
+++ Formula/feyn_rules.rb
@@ -1,12 +1,19 @@
-class Feynrules < Formula
+class FeynRules < Formula
   desc "Mathematica Package"
   homepage "https://feynrules.irmp.ucl.ac.be/"
   url "http://feynrules.irmp.ucl.ac.be/downloads/feynrules-current.tar.gz"
   version "2.3"
   sha256 "9ca695d10ba5073571a7b6f7933f3745c6d7e6f4ee4e925f424e59e3198c200a"
 
-  def install
-    system "./configure"
+ def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
     system "make", "install"
+  end
+
+  test do
+    system "true"
   end
 end
